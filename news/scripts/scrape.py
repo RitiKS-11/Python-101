4a77922bfe668355236ff23d557f9061d28d2070
RitiKSIBJr <letter.ritik011@gmail.com>
2024-01-21 15:58:51 +0545

    indentation

--- news/scripts/scrape.py
+++ news/scripts/scrape.py
@@ -7,7 +7,8 @@ def get_response(url):
 
 
 def scrape_content(response):
-    news = response.text.split('<div class="ssrcss-1u2in0b-Container-ContributorDetails e8mq1e913"><div class="ssrcss-68pt20-Text-TextContributorName e8mq1e96">By Yolande Knell</div><div class="ssrcss-84ltp5-Text e8mq1e910">BBC News Middle East correspondent</div></div></div></div><div class="ssrcss-jlwt2c-Divider e8mq1e915"></div></div><div data-component="text-block" class="ssrcss-11r1m41-RichTextComponentWrapper ep2nwvo0"><div class="ssrcss-7uxr49-RichTextContainer e5tfeyi1"><p class="ssrcss-1q0x1qg-Paragraph e1jhz7w10"><b class="ssrcss-hmf8ql-BoldText e5tfeyi3">')[1].split('</p></div></div><div data-component="topic-list" class="ssrcss-1qmkvfu-TopicListWrapper etw6iwl1">')[0]
+    news = response.text.split('<div data-component="text-block" class="ssrcss-11r1m41-RichTextComponentWrapper ep2nwvo0"><div class="ssrcss-7uxr49-RichTextContainer e5tfeyi1"><p class="ssrcss-1q0x1qg-Paragraph e1jhz7w10"><b class="ssrcss-hmf8ql-BoldText e5tfeyi3">')[1]
+    news = news.split('</p></div></div><div data-component="topic-list" class="ssrcss-1qmkvfu-TopicListWrapper etw6iwl1">')[0]
     text_inside_angle_brackets = r'<(.*?)>'
     clean_news = re.sub(text_inside_angle_brackets, '', news)
    
@@ -16,7 +17,7 @@ def scrape_content(response):
 def save_content(news):
 
     with open('news.json', 'a') as file:
-        json.dump(news, file)
+        json.dump(news, file, indent=4)
 
 
 if __name__ == "__main__":
