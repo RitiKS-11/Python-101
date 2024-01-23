39ed6df2111d35f1ae86618b4b1e29792022b2c3
RitiKSIBJr <letter.ritik011@gmail.com>
2024-01-23 21:44:26 +0545

    fix error

--- parse/pkg/merger.py
+++ parse/pkg/merger.py
@@ -5,54 +5,55 @@ import csv
 def parse_content(filepath):
     with open(filepath) as file:
         district, kpi = file.readline().replace('\n', '').replace(' ', '').split('|')
-        list1 = []
-        list2 = []
+        list1, list2 = [], []
 
         for line in file:
-            line = line.replace('\n', '').replace(' ', '')
-            data = line.split('|')
+            data = line.replace('\n', '').split('|')
 
-            if district.rstrip() in data:
-                district, kpi = data[0], data[1]
+            if district in [d.strip() for d in data]:
+                district, kpi = data[0].strip(), data[1].strip()
                 continue
+            
+            if len(data) == 2:
+                val1, val2 = data[0].strip(), data[1].strip()
 
-            if len(data) == 2 and len(list1) < 3:
-                list1.append({district:data[0], kpi:data[1]})
-
-            elif len(data) == 2:
-                list2.append({district:data[0], kpi:data[1]})
+                if  len(list1) < 3:
+                    list1.append({district:val1, kpi:val2})
+                else:
+                    list2.append({district:val1, kpi:val2})
 
         return (list1, list2)
     
 
 def merge_list(list1, list2):
-    merged_list = []
-    new_dict = defaultdict()
+    new_dict = defaultdict(dict)
 
     for item in list1 + list2:
-        for key, value in item.items():
-            new_dict[key] = value
+        new_dict[item['District']] =  new_dict[item['District']] | item 
 
-        if len(new_dict) == 3:
-            merged_list.append(dict(new_dict))
+    for key, value in new_dict.items():
+        value.setdefault('KPI_1', None)
+        value.setdefault('KPI_2', None)
+            
+        new_dict[key] = new_dict[key] | value
 
-    return merged_list
+    return new_dict
 
 
 def write_csv(result):
     with open('merger.csv', 'a') as file:
         fields = ['District', 'KPI_1', 'KPI_2']
-        writer = csv.DictWriter(file, fieldnames=fields)
+        writer = csv.DictWriter(file, fieldnames=fields) 
 
         writer.writeheader()
 
-        for item in result:
-            writer.writerow(item)
+        for _, value in result.items():
+            writer.writerow(value)
+
 
 
 if __name__ == "__main__":
     list1, list2 = parse_content("merger.txt")
-    print(list1, list2)
     result = merge_list(list1, list2)
     write_csv(result)
 
