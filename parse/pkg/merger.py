533c8572674361c5c5a590089677f689f6abee45
RitiKSIBJr <letter.ritik011@gmail.com>
2024-01-25 13:15:11 +0545

    code for handling different spellings

--- parse/pkg/merger.py
+++ parse/pkg/merger.py
@@ -5,24 +5,24 @@ from nltk.metrics import edit_distance
 def parse_content(filepath):
     with open(filepath) as file:
         district, kpi = file.readline().replace('\n', '').replace(' ', '').split('|')
-        list1, list2 = [], []
+        kpi1, kpi2 = [], []
 
         for line in file:
             data = line.replace('\n', '').split('|')
-
-            if district in [d.strip() for d in data]:
-                district, kpi = data[0].strip(), data[1].strip()
-                continue
             
             if len(data) == 2:
-                val1, val2 = data[0].strip(), data[1].strip()
+                value1, value2 = data[0].strip(), data[1].strip()
 
-                if  len(list1) < 3:
-                    list1.append({district:val1, kpi:val2})
+                if district == value1:
+                    district, kpi = value1, value2
+                    continue
+                
+                if  len(kpi1) < 3:
+                    kpi1.append({district:value1, kpi:value2})
                 else:
-                    list2.append({district:val1, kpi:val2})
+                    kpi2.append({district:value1, kpi:value2})
 
-        return (list1, list2)
+        return (kpi1, kpi2)
     
 
 def merge_list(combined_list):
@@ -30,8 +30,8 @@ def merge_list(combined_list):
     # combined_list = list1 + list2
 
     for item in combined_list:
-            district = item['District']
-            new_dict[district] =  new_dict[district] | item 
+        district = item['District']
+        new_dict[district] =  new_dict[district] | item 
 
 
     for key, value in new_dict.items():
@@ -50,44 +50,52 @@ def write_csv(result):
 
         writer.writeheader()
 
-        for _, value in result.items():
-            writer.writerow(value)
+        for _, result in result.items():
+            writer.writerow(result)
 
 
 def fix_word(key, key2):
     distance = edit_distance(key, key2)
     combined_word = ''
+    fixed_words = []
 
     if distance <= 1:
-
         for i in range(len(key2) + 1):
             candidate_word = key[:i] + key2[i:]
             distance = edit_distance(key2, candidate_word)
 
-            if distance == 1:
-                combined_word = candidate_word
+            if distance <= 1:
+                fixed_words.append(candidate_word)
+        
+        fixed_words = set(fixed_words)
 
+        if key in fixed_words:
+            combined_word =  key
+        elif key2 in fixed_words:
+            combined_word = key2
 
-        return combined_word
+    return combined_word
+
     
 
 def update_combine_list(list1, list2):
-    combined_list = sorted(list1 + list2, key=lambda x: x['District'])
-    new_list = []
-    for index, d in enumerate(combined_list):
+    combined_list = sorted(list1 + list2, key=lambda x: x['District'], reverse=True)
+    updated_combine_list = list()
+
+    for index, data in enumerate(combined_list):
         if (index+1) < len(combined_list):
-            ne_w = fix_word(combined_list[index+1]['District'], d['District'])
+            ne_w = fix_word(combined_list[index+1]['District'], data['District'])
 
-        if ne_w:
-            d['District'] = ne_w
+            if ne_w:
+                data['District'] = ne_w
+                combined_list[index+1]['District'] = ne_w
+                
+        updated_combine_list.append(data)
 
-        new_list.append(d)
-
-    return new_list
+    return updated_combine_list
 
 if __name__ == "__main__":
     list1, list2 = parse_content("merger.txt")
     combined_list = update_combine_list(list1, list2)
     result = merge_list(combined_list)
     write_csv(result)
-    # print(result)
