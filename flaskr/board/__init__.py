98226bfdafc2368271ada6110f8863109d989845
RitiKSIBJr <letter.ritik011@gmail.com>
2024-01-28 16:54:52 +0545

    learning flask

---
+++ flaskr/board/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+from board import pages, errors
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(pages.bp)
+    app.register_error_handler(404, errors.page_not_found)
+
+    return app
