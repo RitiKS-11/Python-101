9d30398ff668976a19dadcdbb357191dc3642a78
RitiKSIBJr <letter.ritik011@gmail.com>
2024-02-05 18:51:11 +0545

    type check added

--- validation/validation/script.py
+++ validation/validation/script.py
@@ -1,22 +1,47 @@
+from cerberus import Validator
 
 
 def validation(dictionary, validation_rule):
     for key, rule in validation_rule.items():
         if key not in dictionary:
             raise "Dict doens't contain the key"
-        
+
         value = dictionary[key]
 
-        if 'bool' in rule and rule['bool'] != value:
-            return False
+        if 'bool' in rule:
+            if not isinstance(rule['bool'], bool):
+                raise ValueError('min_length should be an integer')
+            if rule['bool'] != value:
+                return False
 
-        if 'min_length' in rule and 'max_length' in rule:
-            if len(value) < rule['min_length'] or len(value) > rule['max_length']:
+        if 'min_length' in rule:
+            if not isinstance(rule['min_length'], int):
+                raise ValueError('min_length should be an integer')
+            if len(value) < rule['min_length']:
                 return False
-            
-        if 'min_value' in rule and 'max_value' in rule:
-            if value < rule['min_value'] or value > rule['max_value']:
+
+        if 'max_length' in rule:
+            if not isinstance(rule['max_length'], int):
+                raise ValueError('max_length should be an integer')
+            if len(value) > rule['max_length']:
                 return False
-            
+
+        if 'min_value' in rule:
+            if not isinstance(rule['min_value'], int):
+                raise ValueError('min_value should be an integer')
+            if value < rule['min_value']:
+                return False
+
+        if 'max_value' in rule:
+            if not isinstance(rule['max_value'], int):
+                raise ValueError('max_value should be an integer')
+            if value > rule['max_value']:
+                return False
+
     return True
 
+    # def cerberus_validator(data, rule):
+    #     v = Validator(rule)
+    #     result = v.validate(data)
+
+    #     return result
