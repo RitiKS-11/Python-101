00ba1e1553217e13de0a4f8069878745be62fb37
RitiKSIBJr <letter.ritik011@gmail.com>
2024-02-24 15:24:22 +0545

    fix:repetative json data

--- scrp/scrp/main.py
+++ scrp/scrp/main.py
@@ -8,7 +8,7 @@ from utils.headers import json_header, request_headers
 def json_request(url, geo, detail, offset, attraction, update_token):
     """ Send a request to get json data """
     cookies, headers, json_data = json_header(url, geo, detail, offset, attraction, update_token)
-    response = requests.post('https://www.tripadvisor.com/data/graphql/ids', cookies=cookies, headers=headers, json=json_data)
+    response = requests.post('https://www.tripadvisor.com/data/graphql/ids', cookies=cookies, headers=headers, json=json_data, timeout=10)
 
     return response.json()
 
@@ -16,10 +16,10 @@ def json_request(url, geo, detail, offset, attraction, update_token):
 def parse_json(json_response):
     """ Retrive required data form the json response """
     content_list = []
-    pr = json_response
-    reviews = (pr[4]['data']['Result'][0]['detailSectionGroups'][0]['detailSections'][0]['tabs'][0]['content'][2:12])
-    for review in reviews:
+    json_data = json_response
+    reviews = (json_data[4]['data']['Result'][0]['detailSectionGroups'][0]['detailSections'][0]['tabs'][0]['content'][2:12])
 
+    for review in reviews:
         if review['__typename'] == 'WebPresentation_ReviewsNoFilterResultsCardWeb':
             continue
 
@@ -65,9 +65,7 @@ def get_geo_info(url):
     try: 
         offset = url.split('-o')[1].split('-')[0]
     except:
-        # print(url.split('-'))
         url = '-'.join(url.split('-')[0:3]) + '-or10-' + '-'.join(url.split('-')[3:]) 
-        # print(url)
         
     return {'geo':geo, 'offset':offset, 'detail':detail, 'attraction':attraction, 'url':url}
 
@@ -120,9 +118,10 @@ def handle_json(geo_info, update_token=None, total_reviews=[]):
     res = json_request(geo_info['url'], geo_info['geo'], geo_info['detail'], geo_info['offset'], geo_info['attraction'], update_token)
     # print(res[0]['data']['Opf_getOnPageFactorsForLocale'][0]['errMessage'])
     no_of_reviews = res[4]['data']['Result'][0]['detailSectionGroups'][0]['detailSections'][0]['tabs'][0]['content'][1]['reviewCountText']['text'].split(' ')[0]
-    update_token = res[4]['data']['Result'][0]['detailSectionGroups'][0]['detailSections'][0]['tabs'][0]['content'][12]['links'][1]['updateLink']['updateToken']
-    print(no_of_reviews)
-    print(len(total_reviews))
+    update_token = res[4]['data']['Result'][0]['detailSectionGroups'][0]['detailSections'][0]['tabs'][0]['content'][12]['links'][2]['updateLink']['updateToken']
+    # print(update_token)
+    # print(no_of_reviews)
+    # print(len(total_reviews))
 
     # print('\n\n')
     # print(res)
@@ -137,7 +136,6 @@ def handle_json(geo_info, update_token=None, total_reviews=[]):
         geo_info['offset'] = new_offset
 
 
-        print(geo_info['url'])
         total_reviews = handle_json(geo_info, update_token, total_reviews)
 
     else: 
@@ -161,7 +159,7 @@ def scrp_main():
         result = json_request(geo=geo_info['geo'], detail=geo_info['detail'], offset=geo_info['offset'], url=url, update_token=None, attraction=geo_info['attraction'])
         # print(result)
         update_token = result[4]['data']['Result'][0]['detailSectionGroups'][3]['detailSections'][0]['tabs'][0]['content'][12]['links'][1]['updateLink']['updateToken']
-        # print(update_token)
+        print(update_token)
 
         result_list = handle_json(geo_info=geo_info, update_token=update_token, total_reviews=[])
     save_json(result_list)
