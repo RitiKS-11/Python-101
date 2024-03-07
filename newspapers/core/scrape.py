c954470ecd3db4ebfb0813ea209869e01bd4bcdb
RitiKSIBJr <letter.ritik011@gmail.com>
2024-03-07 23:03:10 +0545

    next page class method

--- newspapers/core/scrape.py
+++ newspapers/core/scrape.py
@@ -16,21 +16,24 @@ class NewsBase:
         return soup
     
     def save_data(self, site, data):
-        filename = site + datetime.now().strftime('%c').replace(' ','_') + '.json'
-        with open(filename, 'w') as file:
+        # filename = site + datetime.now().strftime('%c').replace(' ','_') + '.json'
+        with open(site+'.json', 'w') as file:
+            # current_content = json.load(file)
             json.dump(data, file, indent=4)
             
 
 class HimalayanTimes(NewsBase):
-    def __init__(self):
+    def __init__(self, url=''):
+        self.current_page = 0
         self.url = 'https://thehimalayantimes.com/search?query=job'
+        self.next_page = ''
+        self.data = []
 
     def parse_content(self):
         parsed_list = []
-        content = self.get_response()
-        soup = self.create_soup(content.text)
-        
-        articles = soup.find_all('article', class_='row animate-box fadeInUp animated-fast')
+        self.content = self.get_response()
+        self.soup = self.create_soup(self.content.text)
+        articles = self.soup.find_all('article', class_='row animate-box fadeInUp animated-fast')
 
         for article in articles:
             tilte = article.find('h3', class_='alith_post_title').text
@@ -39,12 +42,25 @@ class HimalayanTimes(NewsBase):
 
             parsed_list.append({'title': tilte, 'description': description, 'url': url})
 
-        self.save_data('himalayantimes',parsed_list )
+        self.data = self.data + parsed_list
+
+        self.current_page = self.soup.find('li', class_='pager-nav active').text
+        self.next_page = self.soup.find_all('li', class_='pager-nav')[int(self.current_page)+1].find('a')['href']
+
+
+        self.save_data('himalayantimes',self.data)
+
+    def next(self):
+        # print(self.next_page)
+        self.url = self.next_page
+        print(self.url)
+        self.parse_content()
 
 
 def process():
     result = HimalayanTimes()
     result.parse_content()
+    result.next()
 
 if __name__ == "__main__":
     process()
