72847487bf7fbf0114a9125322b86a50462a8a45
RitiKSIBJr <letter.ritik011@gmail.com>
2024-03-08 09:07:08 +0545

    continue scrpaing from next page

--- newspapers/core/scrape.py
+++ newspapers/core/scrape.py
@@ -1,4 +1,4 @@
-import requests
+import requests, os
 from bs4 import BeautifulSoup
 import json
 from datetime import datetime
@@ -15,24 +15,47 @@ class NewsBase:
         soup = BeautifulSoup(response_text, 'html.parser')
         return soup
     
-    def save_data(self, site, data):
+    def save_data(self, site, data, page):
+        if os.path.isfile(site+'.json'):
+            with open(site+'.json', 'r') as file:
+                contents = json.load(file)
+
+            contents = contents + data
+            contents[0]['current_page'] = page
+        else:
+            contents = data
+            contents.insert(0, {'current_page':page})
+
+
         # filename = site + datetime.now().strftime('%c').replace(' ','_') + '.json'
         with open(site+'.json', 'w') as file:
-            # current_content = json.load(file)
-            json.dump(data, file, indent=4)
-            
+            json.dump(contents, file, indent=4)
+
+    def get_page(self, site):
+        if os.path.isfile(site+'.json'):
+            with open(site+'.json', 'r') as file:
+                contents = json.load(file)
+                
+                if 'current_page' in contents[0].keys():
+                    return contents[0]['current_page']
+                return 1
+        return 1    
+        
 
 class HimalayanTimes(NewsBase):
     def __init__(self, url=''):
-        self.current_page = 0
-        self.url = 'https://thehimalayantimes.com/search?query=job'
-        self.next_page = ''
+        self.page_no = self.get_page('himalayantimes')
+        self.url = f'https://thehimalayantimes.com/search?query=job&pgno={self.page_no}'
         self.data = []
 
     def parse_content(self):
         parsed_list = []
         self.content = self.get_response()
         self.soup = self.create_soup(self.content.text)
+
+        current_page = self.soup.find('li', class_='pager-nav active').text
+        print(current_page)
+        # self.next_page = self.soup.find_all('li', class_='pager-nav')[int(self.current_page)+1].find('a')['href']
         articles = self.soup.find_all('article', class_='row animate-box fadeInUp animated-fast')
 
         for article in articles:
@@ -44,11 +67,10 @@ class HimalayanTimes(NewsBase):
 
         self.data = self.data + parsed_list
 
-        self.current_page = self.soup.find('li', class_='pager-nav active').text
-        self.next_page = self.soup.find_all('li', class_='pager-nav')[int(self.current_page)+1].find('a')['href']
+        
+        print(len(self.data))
 
-
-        self.save_data('himalayantimes',self.data)
+        self.save_data('himalayantimes',self.data, int(current_page) + 1)
 
     def next(self):
         # print(self.next_page)
@@ -60,7 +82,10 @@ class HimalayanTimes(NewsBase):
 def process():
     result = HimalayanTimes()
     result.parse_content()
-    result.next()
+    # result.next()
 
 if __name__ == "__main__":
     process()
+    # r = NewsBase()
+    # a =  r.get_page('himalayantimes')
+    # print(a)
