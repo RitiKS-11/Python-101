1eb0e0ac450b914abf187d48885cced2f0e62491
RitiKSIBJr <letter.ritik011@gmail.com>
2024-03-25 17:02:17 +0545

    json to csv

--- newspapers/core/analyze.py
+++ newspapers/core/analyze.py
@@ -1,6 +1,11 @@
-import spacy
 import json, os
 from collections import defaultdict
+import csv
+
+import dateparser
+import spacy
+
+
 
 def create_nlp(text):
     nlp = spacy.load('en_core_web_sm')
@@ -45,6 +50,11 @@ def extract_data(content):
 
     return dict(result)
 
+def get_source(url):
+    source = url.split('.')[0].replace('https://','')
+    return source
+
+
 
 def handle_process():
     contents = get_article('himalayantimes/Belt+and+Road+Initiative_content.json')
@@ -57,18 +67,25 @@ def handle_process():
 
         new_reuslt['url'] = content['url']
         new_reuslt['title'] = content['title']
-        new_reuslt['published_date'] = content['published_date']
+        new_reuslt['published_date'] = format_datetime(content['published_date'])
         new_reuslt['content'] = content['content']
         new_reuslt['anlayzed'] = reomve_repetative_data(result)
+        new_reuslt['source'] = get_source(content['url'])
 
 
-        print(new_reuslt)
-        print('\n')
+        # print(new_reuslt)
+        # print('\n')
         result_list.append(new_reuslt)
     save_json('processed.json', result_list)
 
 
+def format_datetime(unformatted_date):
+    date = unformatted_date
+    date = date.replace('Published:', '').strip()
 
+    formatted_date = dateparser.parse(date).strftime("%Y-%m-%d %H:%M")
+    print(formatted_date)
+    return formatted_date
 
 
 def reomve_repetative_data(total_results):
@@ -79,10 +96,35 @@ def reomve_repetative_data(total_results):
     return unique_results
     
     
+def convert_to_csv(filepath):
+    with open(filepath) as file:
+        results = json.load(file)
+
+    fieldnames = ['url', 'title', 'published_date', 'content', 'analyzed_date', 'analyzed_gpe', 'analyzed_org', 'analyzed_person', 'source']
+
+    filepath = filepath.replace('.json', '.csv')
+    with open(filepath, 'w') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for result in results:
+            
+            data = {
+                'url': result['url'], 
+                'title': result['title'], 
+                'published_date': result['published_date'], 
+                'content':result['content'], 
+                'source': result['source']
+
+            }
+            analyzed_dict = {f'analyzed_{key.lower()}':result['anlayzed'][key] for key in result['anlayzed'].keys()}
+            data = data | analyzed_dict
+            print(data)
+            writer.writerow(data)
+        
 
 
 if __name__ == "__main__":
-    handle_process()
-    
+    # handle_process()
+    convert_to_csv('/home/ritik/workspace/python-101/newspapers/processed.json')
 
 # Person, organization, location dates
